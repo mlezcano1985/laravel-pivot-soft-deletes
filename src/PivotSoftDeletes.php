2bf63bb5aeebe0513902713b1bdf7d4eb114b547
mlezcano <mlezcano@itopia.com>
2017-12-11 10:16:10 -0500

    checked if use softdeteles trait

--- src/PivotSoftDeletes.php
+++ src/PivotSoftDeletes.php
@@ -4,17 +4,19 @@ namespace Mlezcano1985\Database\Support;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 trait PivotSoftDeletes
 {
     /**
-     * SoftDelete for Pivot table.
+     * SoftDelete for BelongsToMany relation.
      *
-     * @param  $related \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     * @param  $related BelongsToMany
      * @param $id int
      * @return int
      */
-    public function detachSoftDelete(BelongsToMany $related, $id)
+    public function detachBelongsToManySoftDelete(BelongsToMany $related, $id)
     {
         $relateKey =  $related->getQualifiedRelatedKeyName();
         $foreignKey =  $related->getQualifiedForeignKeyName();
@@ -30,11 +32,53 @@ trait PivotSoftDeletes
     }
 
     /**
-     * Delete the model from the database.
+     * SoftDeletes pivot model if not use a custom pivot model
+     * @param $related Pivot
+     * @return bool|null
+     * */
+    public function detachPivotSoftDelete(Pivot $related)
+    {
+        $relateKey =  $related->getRelatedKey();
+        $foreignKey =  $related->getForeignKey();
+        $tableName = $related->getTable();
+        $deteteAt = $related->getTable().'.deleted_at';
+        $relatedId = $related->attributes[$relateKey];
+        $foreignId = $related->attributes[$foreignKey];
+
+        return DB::table($tableName)
+            ->where($relateKey, $relatedId)
+            ->where($foreignKey, $foreignId)
+            ->update([
+                $deteteAt => Carbon::now()
+            ]);
+    }
+
+    /**
+     * SoftDeletes pivot model.
      * @return bool|null
      * */
     public function detach()
     {
-        return $this->getRelation('pivot')->delete();
+        $related = $this->getRelation('pivot');
+        if($this->useSoftDeletes($related))
+        {
+            return $related->delete();
+        }
+        return $this->detachPivotSoftDelete($related);
+    }
+
+    /**
+     * Determine if the current model implements soft deletes.
+     * @param $related Pivot
+     * @return bool
+     */
+    protected function useSoftDeletes(Pivot $related = null)
+    {
+        $instance = $this;
+        if($related)
+        {
+            $instance = $related;
+        }
+        return method_exists($instance, 'runSoftDelete');
     }
 }
